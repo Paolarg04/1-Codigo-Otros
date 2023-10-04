114cfd5b7890f23ad8f36f588d51af5308e43d6e
PaolaRabago <anaap.94@hotmail.com>
2023-10-04 10:37:49 -0700

    se realiza modificaciones al ejercicio de codigo otros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const baseEndpoint = 'https://api.github.com'; //api de github
+const usersEndpoint = `${baseEndpoint}/users`; //concatena la api de github el /user es para entra a la api dentro de la pag de github
+const n = document.querySelector('.name'); //llama clase en html (a estas variables se les elimina el signo $ por buena practica)
+const b = document.querySelector('.blog'); //se cambia nombre de variable conforme a clase en html 
+const l = document.querySelector('.location'); //variable correcta, se crea clase en html
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+async function displayUser(username) { //funcion displayUser que recibe nombre de usuario
+  n.textContent = 'cargando...'; //solicitud para cambiar texto de variable
+  const response = await fetch (`${usersEndpoint}/${username}`); //se hace una solicitud para obtener informacion del usuario, con el nombre de usuario especificado
+  const data = await response.json (); //data se utiliza para guardar la informacion que recaba del fetch y lo convierte a json
+  console.log(data); //imprime response como objeto json
+  n.textContent = `${data.name}`;
+  b.textContent = `${data.blog}`;
+  l.textContent = `${data.location}`;
 }
 
-function handleError(err) {
+function handleError(err) { 
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}` //se cambian las comillas dobles por backtics, las cuales se utilizan para icorporar variables y texto
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); //se llaman las 2 funciones, en caso que no funcione se despliega el error
